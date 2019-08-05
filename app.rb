1e0ec38f88cbbf8c5ad423a2f799bb5329fbd54a
Courtney Dyson <courtneydyson87@gmail.com>
2019-08-05 11:42:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        "<h1>Let's Bowl!</h1>"
+        erb :index
+    end
+
+    get '/hours' do
+        "Bowling Alley Hours"
+        erb :hours
+    end
+
+    get '/' do
+        "Dates of Tournaments"
+        erb :dates
+    end
+
+    get '/' do
+        "Bar Menu"
+        erb :menu
+    end
 
 end
